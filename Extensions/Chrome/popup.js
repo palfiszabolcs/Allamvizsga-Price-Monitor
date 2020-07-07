5c1e0f905ae897f43a1934a216a5d3cfc94cbb44
palfiszabolcs <palfiszabolcs@icloud.com>
2020-07-07 16:52:38 +0300

    data retrival from database

--- Extensions/Chrome/popup.js
+++ Extensions/Chrome/popup.js
@@ -18,17 +18,36 @@ const config2 = {
 
 firebase.initializeApp(config);
 var database = firebase.database();
+var header = document.getElementById('header');
+var content = document.getElementById('content');
 
-function get_URL(){
+function show_URL(){
    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-      var url = tabs[0].url;
-
-      var content = document.getElementById('content');
-      var p = document.createElement("p");
-      var text = document.createTextNode(url);
+      let url = tabs[0].url;
+      // var content = document.getElementById('content');
+      let p = document.createElement("p");
+      p.setAttribute("class", "text-center");
+      let text = document.createTextNode(url);
       p.appendChild(text);
-      content.appendChild(p);
+      header.appendChild(p);
+   });
+}
 
+function upload_URL(user = "TEST-user", url = "test-url", category = "test category"){
+   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+      let url = tabs[0].url;
+      // // var content = document.getElementById('content');
+      // let p = document.createElement("p");
+      // p.setAttribute("class", "text-center");
+      // let text = document.createTextNode(url);
+      // p.appendChild(text);
+      // header.appendChild(p);
+
+      var postsRef = database.ref('/test').child(user);
+      postsRef.push().set({
+         category: category,
+         url : url
+      });
       // console.log("url in getURL: " + url);
    });
 }
@@ -41,11 +60,34 @@ function push_to_new(user = "TEST-user", url = "test-url", category = "test cate
     });
 }
 
+function read_users_test(){
+   // var userId = firebase.auth().currentUser.uid;
+   database.ref('/test').on("value", function(item){
+      // console.log(item.val()["TEST-user"]);
+      let items = item.val();
+      for(let id of Object.keys(items["TEST-user"])){
+         let url = items["TEST-user"][id].url;
+         let cat = items["TEST-user"][id].category;
+         console.log("product url = " + url);
+         console.log("product cat = " + cat);
+      }
+   },function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+   // let p = document.createElement("p");
+   // let text = document.createTextNode(url);
+   // p.appendChild(text);
+   // content.appendChild(p);
+}
+
 // push_to_new('TEST-user', 'www.proba.url', 'sport');
 
 // document.addEventListener("DOMContentLoaded", push_to_new('feri', get_URL(), 'sports'));
-document.addEventListener("DOMContentLoaded", get_URL());
+document.addEventListener("DOMContentLoaded", show_URL());
 document.getElementById("upload_button").onclick = function(){
-   push_to_new('TEST-user', 'www.proba.url', 'sport');
+   upload_URL();
+}
+document.getElementById("download_button").onclick = function(){
+   read_users_test();
 }
 
